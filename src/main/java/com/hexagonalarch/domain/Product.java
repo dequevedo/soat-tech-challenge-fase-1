adf1bf19aed14839cb49fd3125763703278ec882
Daniel Quevedo <danielelvisquevedo@gmail.com>
2024-10-22 20:20:09 -0300

    Minor formatting

--- src/main/java/com/hexagonalarch/domain/Product.java
+++ src/main/java/com/hexagonalarch/domain/Product.java
@@ -14,9 +14,13 @@ import lombok.NoArgsConstructor;
 public class Product {
 
     private Long id;
+
     private String name;
+
     private String description;
+
     private Double price;
+
     private ProductCategory category;
 
     public static Product valueOf(CreateProductRequest createProductRequest) {
