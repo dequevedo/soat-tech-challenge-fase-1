d2fe14c021e238ba0c3dcf6a92de4ee50d95e4e2
Daniel Quevedo <danielelvisquevedo@gmail.com>
2024-11-05 21:46:32 -0300

    feat: Fix Create Order Response

--- src/main/java/com/hexagonalarch/adapters/converters/OrderConverter.java
+++ src/main/java/com/hexagonalarch/adapters/converters/OrderConverter.java
@@ -1,8 +1,6 @@
 package com.hexagonalarch.adapters.converters;
 
 import com.hexagonalarch.adapters.dto.request.CreateOrderRequest;
-import com.hexagonalarch.adapters.dto.response.CreateOrderResponse;
-import com.hexagonalarch.adapters.dto.response.CreateProductResponse;
 import com.hexagonalarch.core.domain.Order;
 import com.hexagonalarch.core.domain.Product;
 import org.springframework.stereotype.Component;
@@ -19,17 +17,4 @@ public class OrderConverter {
         return new Order(null, request.getCustomerId(), null, products);
     }
 
-    public CreateOrderResponse toResponse(Order order) {
-        List<CreateProductResponse> products = order.getProducts().stream()
-                .map(product -> CreateProductResponse.builder()
-                        .id(product.getId())
-                        .name(product.getName())
-                        .description(product.getDescription())
-                        .price(product.getPrice())
-                        .category(product.getCategory())
-                        .build())
-                .collect(Collectors.toList());
-
-        return new CreateOrderResponse(order.getId(), order.getCustomerId(), order.getStatus(), products);
-    }
 }
--- src/main/java/com/hexagonalarch/adapters/converters/ProductConverter.java
+++ src/main/java/com/hexagonalarch/adapters/converters/ProductConverter.java
@@ -1,6 +1,7 @@
 package com.hexagonalarch.adapters.converters;
+
 import com.hexagonalarch.adapters.dto.request.CreateProductRequest;
-import com.hexagonalarch.adapters.dto.response.CreateProductResponse;
+import com.hexagonalarch.adapters.outbound.jpa.entity.CategoryEntity;
 import com.hexagonalarch.adapters.outbound.jpa.entity.ProductEntity;
 import com.hexagonalarch.core.domain.Product;
 import com.hexagonalarch.core.domain.ProductCategory;
@@ -14,20 +15,25 @@ public class ProductConverter {
                 request.getPrice(), new ProductCategory(request.getCategory()));
     }
 
-    public CreateProductResponse toResponse(Product product) {
-        return CreateProductResponse.builder()
-                .id(product.getId())
-                .name(product.getName())
-                .description(product.getDescription())
-                .price(product.getPrice())
-                .category(product.getCategory())
-                .build();
-    }
-
     public Product entityToDomain(ProductEntity productEntity) {
         if (productEntity == null) return null;
         ProductCategory category = new ProductCategory(productEntity.getCategory().getName());
         return new Product(productEntity.getId(), productEntity.getName(), productEntity.getDescription(),
                 productEntity.getPrice(), category);
+    }
+
+    public ProductEntity domainToEntity(Product product) {
+        if (product == null) return null;
+
+        CategoryEntity categoryEntity = new CategoryEntity();
+        categoryEntity.setName(product.getCategory().getValue());
+
+        return ProductEntity.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .category(categoryEntity)
+                .build();
     }
 }
